937297a75877fbf47b752d4d7e403e0327ca7e65
NithishkumarA <nk0283@srmist.edu.in>
2026-03-10 20:37:02 +0530

    First commit

---
+++ ML/practice.py
@@ -0,0 +1,37 @@
+import math
+import numpy as np
+# data = [10, 20, 30, 40, 50]
+# max = max(data)
+# min = min(data)
+# sumi = sum(data)
+# mean = sumi/len(data)
+
+# variance =(sum((x-mean)**2 for x in data))/len(data)
+# std_deviation = math.sqrt(variance)
+
+# print(max,min,sumi,mean,variance,std_deviation)
+
+# a = np.array([1, 2, 3])
+# b = np.array([4, 5, 6])
+# c=np.dot(a,b)
+# print(a+b)
+# print(c)
+
+# x = np.array([1,2,3,4,5,6])
+# w=3
+# b=1
+
+
+# y=w*x+b
+# print(y)
+
+# z = np.array([10])
+# print(x+z)
+
+# a = np.array([[1,2,3],
+#               [4,5,6]])
+
+
+# b = a.reshape(2,-1)
+# print(b)
+# print(a.T)
