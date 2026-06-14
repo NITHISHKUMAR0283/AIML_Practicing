82a0945f6dfd0d4577e0af125f092e12c957f392
NithishkumarA <nk0283@srmist.edu.in>
2026-06-14 09:33:51 +0530

    notes for cross entropy loss and adam optimisation

--- DL/practice/practice.py
+++ DL/practice/practice.py
@@ -1,50 +1,53 @@
-import torch 
-from torchvision import datasets
-from torchvision import transforms
-from torch.utils.data import DataLoader
-import torch.nn as nn
+# import torch 
+# from torchvision import datasets
+# from torchvision import transforms
+# from torch.utils.data import DataLoader
+# import torch.nn as nn
 
-class NeuralNet(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = nn.Linear(784,128)
-        self.fc2 = nn.Linear(128,64)
-        self.fc3 = nn.Linear(64,10)
-    def forward(self,x):
-        x = x.reshape(-1,784)
-        x = self.fc1(x)
-        x = self.fc2(x)        
-        x = self.fc3(x)
-        return x
-
-
-model = NeuralNet()
+# class NeuralNet(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.fc1 = nn.Linear(784,128)
+#         self.fc2 = nn.Linear(128,64)
+#         self.fc3 = nn.Linear(64,10)
+#     def forward(self,x):
+#         x = x.reshape(-1,784)
+#         x = self.fc1(x)
+#         x = self.fc2(x)        
+#         x = self.fc3(x)
+#         return x
 
 
+# model = NeuralNet()
 
-transform = transforms.ToTensor()
 
-train_dataset = datasets.MNIST(
-    root="./data",
-    train=True,
-    transform = transform,
-    download = True
-)
-test_dataset = datasets.MNIST(
-    root = "./data",
-    train=False,
-    transform = transform,
-    download = True
-)
 
-train_loader = DataLoader(
-    train_dataset,
-    batch_size = 32,
-    shuffle = True
-)
+# transform = transforms.ToTensor()
 
-image , label = next(iter(train_loader))
+# train_dataset = datasets.MNIST(
+#     root="./data",
+#     train=True,
+#     transform = transform,
+#     download = True
+# )
+# test_dataset = datasets.MNIST(
+#     root = "./data",
+#     train=False,
+#     transform = transform,
+#     download = True
+# )
 
-output = model(image)
-print(output.shape)
-print(output[0])
+# train_loader = DataLoader(
+#     train_dataset,
+#     batch_size = 32,
+#     shuffle = True
+# )
+
+# image , label = next(iter(train_loader))
+
+# output = model(image)
+# print(output.shape)
+# print(output[0])
+
+
+
